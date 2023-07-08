d30ba83fa9f7f6642ffd40f0d08b679d1c543b25
Shubham Patrick <ahthneeuhl@gmail.com>
2023-07-08 15:59:41 +0530

    feat: Load the blender Model

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,27 +1,72 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
 
-import { Center, OrbitControls } from "@react-three/drei";
+import {
+  AccumulativeShadows,
+  Center,
+  OrbitControls,
+  RandomizedLight,
+  useGLTF,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
-function Shirt() {
+function Shirt(props) {
+  const { nodes, materials } = useGLTF("/shirt_baked_collapsed.glb");
+
   return (
-    <mesh>
-      <boxGeometry args={[0.9, 0.9, 0.9]} />
-      <meshNormalMaterial />
-    </mesh>
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/shirt_baked_collapsed.glb");
+
+function Backdrop() {
+  return (
+    <AccumulativeShadows
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={10}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.14]}
+    >
+      <RandomizedLight
+        amount={4}
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
+      <RandomizedLight
+        amount={4}
+        radius={5}
+        intensity={0.25}
+        ambient={0.55}
+        position={[-5, 5, -9]}
+      />
+    </AccumulativeShadows>
   );
 }
 
 const App = ({ position = [-1, 0, 2.5], fov = 25 }) => {
   return (
     <Canvas
+      shadows
       eventSource={document.getElementById("root")}
       eventPrefix="client"
       camera={{ position, fov }}
     >
+      <ambientLight intensity={0.5} />
       <Center>
         <Shirt />
+        <Backdrop />
       </Center>
       <OrbitControls />
     </Canvas>
