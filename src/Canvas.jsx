c303b14f45844e99e65d2cd2dee67008a964f706
Shubham Patrick <ahthneeuhl@gmail.com>
2023-07-08 14:57:10 +0530

    chore: Create the basic React Three Fiber Scene

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,9 +1,31 @@
-const Canvas = () => {
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unknown-property */
+
+import { Center, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+function Shirt() {
   return (
-    <div>
-      <h1>Canvas</h1>
-    </div>
+    <mesh>
+      <boxGeometry args={[0.9, 0.9, 0.9]} />
+      <meshNormalMaterial />
+    </mesh>
+  );
+}
+
+const App = ({ position = [-1, 0, 2.5], fov = 25 }) => {
+  return (
+    <Canvas
+      eventSource={document.getElementById("root")}
+      eventPrefix="client"
+      camera={{ position, fov }}
+    >
+      <Center>
+        <Shirt />
+      </Center>
+      <OrbitControls />
+    </Canvas>
   );
 };
 
-export default Canvas;
+export default App;
