9b251fc992dfa67170013ff8bec6eae95b754657
Shubham Patrick <ahthneeuhl@gmail.com>
2023-07-08 17:28:40 +0530

    feat: Create the Customizer Overlay

--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -1,39 +1,94 @@
 /* eslint-disable react/no-unescaped-entities */
 
 import { Logo } from "@pmndrs/branding";
-import { AiOutlineShopping, AiOutlineHighlight } from "react-icons/ai";
+import {
+  AiOutlineShopping,
+  AiOutlineHighlight,
+  AiFillCamera,
+  AiOutlineArrowLeft,
+} from "react-icons/ai";
 
 export default function Overlay() {
-  return <Intro />;
+  return (
+    <div className="container">
+      <header>
+        <Logo width="40" height="40" />
+        <div>
+          <AiOutlineShopping size="3em" />
+        </div>
+      </header>
+      <Intro />
+      <Customizer />
+    </div>
+  );
 }
 
 function Intro() {
   return (
-    <div className="container">
-      <header>
-        <Logo width={40} height={40} />
-        <AiOutlineShopping size="3em" />
-      </header>
-
-      <section key="main">
-        <div className="section--container">
+    <section key="main">
+      <div className="section--container">
+        <div>
+          <h1>LET'S DO IT.</h1>
+        </div>
+        <div className="support--content">
           <div>
-            <h1>LET'S DO IT.</h1>
-          </div>
-          <div className="support--content">
-            <div>
-              <p>
-                Create your unique and exclusive shirt with our brand-new 3D
-                customization tool. <strong>Unleash your imagination</strong>{" "}
-                and define your own style.
-              </p>
-              <button style={{ background: "black" }}>
-                CUSTOMIZE IT <AiOutlineHighlight size="1.3em" />
-              </button>
-            </div>
+            <p>
+              Create your unique and exclusive shirt with our brand-new 3D
+              customization tool. <strong>Unleash your imagination</strong> and
+              define your own style.
+            </p>
+            <button style={{ background: "black" }}>
+              CUSTOMIZE IT <AiOutlineHighlight size="1.3em" />
+            </button>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
+  );
+}
+
+function Customizer() {
+  const colors = [
+    "#CCCCCC",
+    "#EFBD4E",
+    "#80C670",
+    "#726DE8",
+    "#EF674E",
+    "#353934",
+    "#800080",
+  ];
+  const decals = ["react", "three2", "pmndrs"];
+
+  return (
+    <section key="custom">
+      <div className="customizer">
+        <div className="color-options">
+          {colors.map((color) => (
+            <div
+              key={color}
+              className="circle"
+              style={{ background: color }}
+            ></div>
+          ))}
+        </div>
+        <div className="decals">
+          <div className="decals--container">
+            {decals.map((decal) => (
+              <div key={decal} className="decal">
+                <img src={decal + "_thumb.png"} alt="brand" />
+              </div>
+            ))}
+          </div>
+        </div>
+        <button className="share" style={{ background: "black" }}>
+          DOWNLOAD
+          <AiFillCamera size="1.3em" />
+        </button>
+        <button className="exit" style={{ background: "black" }}>
+          GO BACK
+          <AiOutlineArrowLeft size="1.3em" />
+        </button>
+      </div>
+    </section>
   );
 }
