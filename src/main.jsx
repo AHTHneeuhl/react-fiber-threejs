e682a7cdcd1a6f153d0b7bd43e80b6f37f6ce5ed
Shubham Patrick <ahthneeuhl@gmail.com>
2023-07-08 17:04:15 +0530

    feat: Create the Intro Overlay

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import Canvas from "./Canvas.jsx";
+import Overlay from "./Overlay.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(<Canvas />);
+createRoot(document.getElementById("root")).render(
+  <>
+    <Canvas />
+    <Overlay />
+  </>
+);
