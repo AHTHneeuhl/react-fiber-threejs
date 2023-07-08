06c3ce86118f53981900bc06a54672f57356119c
Shubham Patrick <ahthneeuhl@gmail.com>
2023-07-08 21:08:54 +0530

    feat: Animate the interface with framer motion

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -90,8 +90,15 @@ function Backdrop() {
 function CameraRig({ children }) {
   const group = useRef();
 
+  const snap = useSnapshot(state);
+
   useFrame((state, delta) => {
-    easing.damp3(state.camera.position, [0, 0, 2], 0.25, delta);
+    easing.damp3(
+      state.camera.position,
+      [snap.intro ? -1.25 : -0.8, 0, 2],
+      0.25,
+      delta
+    );
     easing.dampE(
       group.current.rotation,
       [state.pointer.y / 10, -state.pointer.x / 5, 0],
@@ -103,7 +110,7 @@ function CameraRig({ children }) {
   return <group ref={group}>{children}</group>;
 }
 
-export default function App({ position = [-1, 0, 2.5], fov = 25 }) {
+export default function App({ position = [-1, 0, 2.5], fov = 24 }) {
   return (
     <Canvas
       shadows
